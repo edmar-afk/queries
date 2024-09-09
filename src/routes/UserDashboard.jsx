131e74b8b21673728dd243de890bca51a4c766de
project2479 <jaywrsnp6@gmail.com>
2024-09-09 17:33:34 +0800

    new

--- src/routes/UserDashboard.jsx
+++ src/routes/UserDashboard.jsx
@@ -1,14 +1,28 @@
+import { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
 import Header from "../components/userDashboard/Header";
 import Search from "../components/userDashboard/Search";
-import Offices from '../components/userDashboard/Offices'
+import Offices from "../components/userDashboard/Offices";
 
 function UserDashboard() {
-	const userData = JSON.parse(localStorage.getItem("userData")) || {};
+	const [redirect, setRedirect] = useState(false);
+
+	useEffect(() => {
+		const userData = JSON.parse(localStorage.getItem("userData")) || {};
+		if (!userData || Object.keys(userData).length === 0) {
+			setRedirect(true); // Trigger redirection
+		}
+	}, []); // Empty dependency array, runs once on mount
+
+	if (redirect) {
+		return <Navigate to="/" />;
+	}
+
 	return (
 		<>
-            <Header />
-            <Search />
-            <Offices/>
+			<Header />
+			<Search />
+			<Offices />
 		</>
 	);
 }
