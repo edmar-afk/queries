229ea217b35c87f53ea98cbaa69843e1ec5af237
project2479 <jaywrsnp6@gmail.com>
2024-12-27 14:46:43 +0800

    new

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,55 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestForPlugin = {
+	registerType: "prompt",
+	includeAssets: ["favicon.ico", "apple-touc-icon.png"],
+	manifest: {
+		name: "JHCSC Student Support",
+		short_name: "JHCSC Student Support",
+		description: "QUERY OF JHCSC",
+		icons: [
+			{
+				src: "./icon-192x192.png",
+				sizes: "192x192",
+				type: "image/png",
+			},
+			{
+				src: "./icon-512x512.png",
+				sizes: "512x512",
+				type: "image/png",
+				purpose: "favicon",
+			},
+			{
+				src: "/apple-touch-icon.png",
+				sizes: "180x180",
+				type: "image/png",
+				purpose: "apple touch icon",
+			},
+			{
+				src: "./icon-144x144.png",
+				sizes: "144x144",
+				type: "image/png",
+				purpose: "any",
+			},
+			{
+				src: "./icon-256x256.png",
+				sizes: "256x256",
+				type: "image/png",
+				purpose: "icon",
+			},
+		],
+		theme_color: "#181818",
+		background_color: "#e8eac2",
+		display: "standalone",
+		scope: "/",
+		start_url: "/",
+		orientation: "portrait",
+	},
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react(), VitePWA(manifestForPlugin)],
+});
